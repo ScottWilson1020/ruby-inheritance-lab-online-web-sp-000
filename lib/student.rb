26987cf063d6a5c1dc7decc1b1430b5174fd0d73
Scott Wilson <scottwilson1020@gmail.com>
2020-08-18 06:57:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 require_relative "./user.rb"
 
 class Student < User 
+  attr_accessor :knowledge
   
   def initialize
-    @knowledge
+    @knowledge = knowledge
   end 
 
+def learn
+  @knowledge << 
+end 
 end
