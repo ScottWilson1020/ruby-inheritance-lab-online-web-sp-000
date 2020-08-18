216d4345419a1f23172ca832eb4f44b4bfb96382
Scott Wilson <scottwilson1020@gmail.com>
2020-08-18 06:57:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,11 @@ class Student < User
   attr_accessor :knowledge
   
   def initialize
-    @knowledge = knowledge
+    @knowledge = []
   end 
 
-def learn
-  @knowledge << 
+def learn(string)
+  @knowledge << string
 end 
 end
+
